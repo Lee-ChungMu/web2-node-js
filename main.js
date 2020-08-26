beca9a79240b1b88d773998079cba4d269070ae5
Lee-ChungMu <69577355+Lee-ChungMu@users.noreply.github.com>
2020-08-26 19:29:19 +0900

    Add files via upload
    19.2 홈페이지 구현

--- main.js
+++ main.js
@@ -4,53 +4,75 @@ var url = require('url');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
+    var pathname =url.parse(_url, true).pathname;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
 
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
+
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descripion){
+          var title = 'Welcome';
+          var descripion = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li><br>
+              </ol>
+
+              <h2>${title}</h2>
+              <p>${descripion}</p>
+            </body>
+          </html>
+
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else{
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descripion){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li><br>
+              </ol>
+
+              <h2>${title}</h2>
+              <p>${descripion}</p>
+            </body>
+          </html>
+
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
+    } else {
       response.writeHead(404);
-      response.end();
-      return;
+      response.end('not found');
     }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descripion){
-      var template = `
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-          <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175329272-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
 
-        gtag('config', 'UA-175329272-1');
-      </script>
-
-        </head>
-
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li><br>
-          </ol>
-
-          <h2>${title}</h2>
-          <p>${descripion}</p>
-        </body>
-      </html>
-
-      `;
-      response.end(template);
-    })
 
 
 });
