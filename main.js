957e33d7d0e73b04dd985fe50dde5242fcb54e7f
Lee-ChungMu <69577355+Lee-ChungMu@users.noreply.github.com>
2020-09-01 21:03:38 +0900

    Add files via upload
    31 form UI 출력

--- main.js
+++ main.js
@@ -1,93 +1,82 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
 
+    <body>
+      <h1><a href="/">WEB</a></h1>
+
+      ${list}
+      <a href="/create">create</a>
+      ${body}
+
+    </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = '<ul>';
+  var i=0;
+  while(i<filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    i=i+1;
+  }
+  list = list + '<ul>';
+  return list;
+}
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var pathname =url.parse(_url, true).pathname;
     var queryData = url.parse(_url, true).query;
 
 
-    if(pathname === '/'){
+    if(pathname === '/'){//홈으로 갔느냐
       if(queryData.id === undefined){
-
       fs.readdir('./data', function(error, filelist){
         var title = 'Welcome';
         var descripion = 'Hello, Node.js';
-        /*
-        var list = `<ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li><br>
-        </ol>`
-        */
-        var list = '<ul>';
-        var i=0;
-        while(i<filelist.length){
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-          i=i+1;
-        }
-        list = list + '<ul>';
-        var template = `
-        <!doctype html>
-        <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-
-          <body>
-            <h1><a href="/">WEB</a></h1>
-
-            ${list}
-
-            <h2>${title}</h2>
-            <p>${descripion}</p>
-          </body>
-        </html>
-
-        `;
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${descripion}`);
         response.writeHead(200);
         response.end(template);
       })
-
-
       } else{
         fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var descripion = 'Hello, Node.js';
-          var list = '<ul>';
-          var i=0;
-          while(i<filelist.length){
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-            i=i+1;
-          }
-          list = list + '<ul>';
+          var list = templateList(filelist);
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, descripion){
             var title = queryData.id;
-            var template = `
-            <!doctype html>
-            <html>
-              <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-              </head>
-
-              <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${descripion}</p>
-              </body>
-            </html>
-
-            `;
+            var template = templateHTML(title, list, `<h2>${title}</h2>${descripion}`);
             response.writeHead(200);
             response.end(template);
           });
         });
       }
-    } else {
+    } else if(pathname === "/create"){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `<form action = "http://localhost:3000/process create" method="post">
+          <p><input type="text" name = "title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+`);
+        response.writeHead(200);
+        response.end(template);
+      })
+    }
+    else {
       response.writeHead(404);
       response.end('not found');
     }
